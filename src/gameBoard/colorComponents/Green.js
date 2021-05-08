c85080de7b6e838309974dc1366adecb50d151a7
Chance Harmon <henrikillsit@gmail.com>
2021-05-07 20:05:00 -0700

    whistle while you work

--- src/gameBoard/colorComponents/Green.js
+++ src/gameBoard/colorComponents/Green.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import { Howl, Howler } from 'howler';
 
+import FirstWhistle from './../../audio/simon-first-note.mp3'
+
+const clips = [
+    { sound: FirstWhistle }
+]
 class Green extends React.Component {
     constructor(props) {
         super(props);
@@ -9,8 +15,18 @@ class Green extends React.Component {
         }
     }
 
+    playSound = (src) => {
+        console.log('in sound')
+        const sound = new Howl({
+            src
+        })
+        console.log(sound)
+        sound.play();
+    }
+
     handleColor = () => {
         console.log('green clicked')
+        this.playSound(clips[0].sound)
         this.setState({ color: 'rgb(156,255,138)', border: 'rgb(222,255,216)' });
         setTimeout(() => {
             this.setState({ color: 'rgb(40,255,0)', border: 'rgb(0,0,0)' });
@@ -20,6 +36,7 @@ class Green extends React.Component {
 
 
     render() {
+        Howler.volume(1.0)
         return (
             <div id='green' style={{ backgroundColor: this.state.color, borderColor: this.state.border }} onClick={this.handleColor}>
             </div>
