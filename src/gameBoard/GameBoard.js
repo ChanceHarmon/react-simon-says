6b7a4971421d707b6d748f1410834964c07dcbb9
Chance Harmon <henrikillsit@gmail.com>
2021-05-12 10:12:56 -0700

    now able to track score, and compare moves vs npc list. Trying to get npc to animate the board next.

--- src/gameBoard/GameBoard.js
+++ src/gameBoard/GameBoard.js
@@ -10,12 +10,14 @@ import SecondWhistle from './../audio/simon-second-note.mp3';
 import ThirdWhistle from './../audio/simon-third-note.mp3';
 import FourthWhistle from './../audio/simon-fourth-note.mp3';
 
+//clips => Part of Howler setup, I have tried it a lot of ways, but array, even if it is only one item seems to be the way. clips could be banana, just what the example from the use case i studied named the variable. objects with a key of sound at each index does not appear to be banana, so I didn't experiment with that
 const clips = [
     { sound: FirstWhistle },
     { sound: SecondWhistle },
     { sound: ThirdWhistle },
     { sound: FourthWhistle }
 ];
+const colorArray = ['green', 'red', 'yellow', 'blue'];
 
 class GameBoard extends React.Component {
     constructor(props) {
@@ -25,6 +27,12 @@ class GameBoard extends React.Component {
             streak: 0,
             npcList: [],
             playerList: [],
+            playBoxText: 'Play',
+            playBoxStyles: {
+                bgColor: 'rgb(0,0,0)',
+                textColor: 'rgb(240, 248, 255)',
+                border: 'rgb(0,0,0)'
+            },
             green: {
                 bgColor: 'rgb(40,255,0)',
                 border: 'rgb(0,0,0)'
@@ -45,6 +53,7 @@ class GameBoard extends React.Component {
         }
     }
 
+    //playSound =>  straight from the Howler docs, basic boiler plate to be able to play a sound function for this Lib
     playSound = (src) => {
         const sound = new Howl({
             src
@@ -52,8 +61,31 @@ class GameBoard extends React.Component {
         sound.play();
     }
 
+    // Made this a switch because why not, seemed like a perfect use case vs if/else if/else
+    handlePlayerColor = (color) => {
+        switch (color) {
+            case 'green':
+                this.handleGreen();
+                this.userPlay(color);
+                break;
+            case 'red':
+                this.handleRed();
+                this.userPlay(color);
+                break;
+            case 'yellow':
+                this.handleYellow();
+                this.userPlay(color);
+                break;
+            case 'blue':
+                this.handleBlue();
+                this.userPlay(color);
+                break;
+            default:
+                console.log('Simon did not say to do this...')
+        }
+    }
 
-    handleColor = (color) => {
+    handleNpcColor = (color) => {
         switch (color) {
             case 'green':
                 this.handleGreen();
@@ -71,6 +103,9 @@ class GameBoard extends React.Component {
                 console.log('Simon did not say to do this...')
         }
     }
+
+    // Below is each function that handles color and border change( and sound ) per click for each color.
+    //Thank you Jacob Knaack for helping me get over the prevState hurdle
 
     handleGreen = () => {
         console.log('green clicked')
@@ -140,15 +175,63 @@ class GameBoard extends React.Component {
         }, 500)
     }
 
+    handleGameStart = () => {
+        this.setState(prevState => {
+            return {
+                ...prevState, playBoxText: `Score: ${this.state.score}`, playBoxStyles: {
+                    textColor: 'rgb(144, 199, 248)', border: 'rgb(240, 248, 255)'
+                }
+            }
+        })
+        this.npcPlay();
+    }
+
+    randomNum = (max) => {
+        return Math.floor(Math.random() * max);
+    }
+
+    npcPlay = () => {
+        this.state.npcList.push(colorArray[this.randomNum(colorArray.length)])
+        this.setState({ playerList: [] })
+        console.log(this.state)
+        // this.userPlay();
+    }
+
+    userPlay = (color) => {
+        this.state.playerList.push(color);
+        let idx = this.state.playerList.length - 1;
+        if (this.state.playerList[idx] !== this.state.npcList[idx]) {
+            console.log('didn\'t match')
+        } else if (this.state.playerList[idx] === this.state.npcList[idx]) {
+            console.log('Match')
+            if (this.state.playerList.length === this.state.npcList.length) {
+                this.handleScore()
+                this.npcPlay()
+            }
+        }
+        console.log(this.state.playerList)
+        // this.npcPlay();
+    }
+
+    handleScore = () => {
+        let newScore = this.state.score + 1;
+        console.log(newScore)
+        if (this.state.streak < newScore) {
+            this.setState({ score: newScore, streak: newScore, playBoxText: `Score: ${newScore}` })
+        } else {
+            this.setState({ score: newScore, playBoxText: `Score: ${newScore}` })
+        }
+    }
+
     render() {
         Howler.volume(1.0)
         return (
             <>
-                <Green handleColor={this.handleColor} bgColor={this.state.green.bgColor} border={this.state.green.border} />
-                <Red handleColor={this.handleColor} bgColor={this.state.red.bgColor} border={this.state.red.border} />
-                <Yellow handleColor={this.handleColor} bgColor={this.state.yellow.bgColor} border={this.state.yellow.border} />
-                <Blue handleColor={this.handleColor} bgColor={this.state.blue.bgColor} border={this.state.blue.border} />
-                <PlayButton />
+                <Green handleColor={this.handlePlayerColor} bgColor={this.state.green.bgColor} border={this.state.green.border} />
+                <Red handleColor={this.handlePlayerColor} bgColor={this.state.red.bgColor} border={this.state.red.border} />
+                <Yellow handleColor={this.handlePlayerColor} bgColor={this.state.yellow.bgColor} border={this.state.yellow.border} />
+                <Blue handleColor={this.handlePlayerColor} bgColor={this.state.blue.bgColor} border={this.state.blue.border} />
+                <PlayButton startGame={this.handleGameStart} boxContent={this.state.playBoxText} boxStyle={this.state.playBoxStyles} />
             </>
         )
     }
